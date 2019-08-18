ac294928ae6d8f2efd609d6ce35c96d84393db27
Nicolas <nrojas@bee.cl>
2019-08-18 17:17:45 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://nrojas:fcqOXDp7ltRV3SLM@cluster0-tg4ai.mongodb.net/cafe';
+    urlDB = 'mongodb+srv://jrojas:vicente3A@cluster0-mjrzc.mongodb.net/cafe';
 }
 process.env.URLDB = urlDB;
